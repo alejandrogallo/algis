1854120b8d870835769a8cd7c297ed40308ed2c8
Alejandro Gallo <aamsgallo@gmail.com>
2019-11-22 15:51:23 +0100

    Implement an expand function

--- src/QuantumField.hs
+++ src/QuantumField.hs
@@ -1,15 +1,17 @@
 module QuantumField where
 
+import Prelude hiding ((*), (+))
+
 type Index = String
 type Field = Double
 
 class Tex a where
   tex :: a -> String
 
-
 data Scalar
   = Delta String String
   | Tensor String Index Index
+  | ParticleSign
   | ScalarZero
   | ScalarOne
   | Val Field
@@ -21,6 +23,7 @@ data Scalar
 data Operator
   = FockOne
   | FockZero
+  | Op Index
   | Particle Index
   | Hole Index
   | Dagger Operator
@@ -29,69 +32,112 @@ data Operator
   | Scaled Scalar Operator
   deriving(Show, Eq)
 
+class Ring a where
+  (*) :: a -> a -> a
+  (+) :: a -> a -> a
+
+instance Ring Operator where
+  (*) = Oprod
+  (+) = Osum
+
 conj :: Scalar -> Scalar
-conj (Delta s t) = (Delta s t)
-conj (Val s) = (Conj s)
-conj (Conj s) = (Val s)
-conj ScalarZero = ScalarZero
-conj ScalarOne = ScalarOne
-conj (Sprod s t) = (Sprod (conj s) (conj t))
-conj (Ssum s t) = (Ssum (conj s) (conj t))
-conj (Tensor s a i) = (Tensor s a i)
+conj (Delta s t)    = Delta s t
+conj (Val  s   )    = Conj s
+conj (Conj s   )    = Val s
+conj ScalarZero     = ScalarZero
+conj ScalarOne      = ScalarOne
+conj (Sprod s t   ) = Sprod (conj s) (conj t)
+conj (Ssum  s t   ) = Ssum (conj s) (conj t)
+conj (Tensor s a i) = Tensor s a i
 
 dag :: Operator -> Operator
 dag (Dagger (Particle i)) = Particle i
-dag (Dagger (Hole a)) = Hole a
-dag (Oprod a b) = (Oprod (dag b) (dag a))
-dag (Scaled s o) = (Scaled s (dag o))
-dag FockOne = FockOne
-dag FockZero = FockZero
-dag o = Dagger o
+dag (Dagger (Hole     a)) = Hole a
+dag (Oprod  a b         ) = Oprod (dag b) (dag a)
+dag (Scaled s o         ) = Scaled s (dag o)
+dag FockOne               = FockOne
+dag FockZero              = FockZero
+dag o                     = Dagger o
+
+isOneBody :: Operator -> Bool
+isOneBody (Op       x           ) = True
+isOneBody (Dagger   (Op x)      ) = True
+isOneBody (Particle x           ) = True
+isOneBody (Dagger   (Particle x)) = True
+isOneBody (Hole     x           ) = True
+isOneBody (Dagger   (Hole x)    ) = True
+isOneBody (Scaled   s p    ) = isOneBody p
+isOneBody (Osum p q) = isOneBody p && isOneBody q
+isOneBody  _ = False
+
+normalize :: Operator -> Operator
+normalize (Oprod (Op b) (Dagger (Op a))) = Scaled ParticleSign o
+  where o = Oprod (Dagger (Op a)) (Op b)
+normalize (Oprod b (Dagger (Op a))) = Scaled ParticleSign o
+  where o = Oprod (Dagger (Op a)) (normalize b)
+normalize (Oprod b (Op a)) = Oprod (normalize b) (Op a)
+normalize (Oprod (Particle b) (Dagger (Particle a))) = Scaled ParticleSign o
+  where o = Oprod (Dagger (Particle a)) (Particle b)
+normalize (Oprod (Dagger (Hole a)) (Hole b)) = Scaled ParticleSign o
+  where o = Oprod (Hole b) (Dagger (Hole a))
+normalize (Osum   a b) = Osum (normalize a) (normalize b)
+normalize (Scaled a b) = Scaled a (normalize b)
+normalize a            = a
+
+expand :: Operator -> Operator
+expand (Oprod (Osum a b) c) = Osum (expand (Oprod a c)) (expand (Oprod b c))
+expand (Oprod c (Osum a b)) = Osum (expand (Oprod c a)) (expand (Oprod c b))
+--expand (Oprod a b) = Qprod (expand a) (expand b)
+expand (Osum a b) = Osum (expand a) (expand b)
+expand c = c
 
 (^+) :: Operator -> Operator
 (^+) = dag
 
-(#) :: Operator -> Operator -> Operator
-(#) = Oprod
-
 instance Tex Operator where
-  tex (Particle x) = "p_{" ++ x ++ "}"
-  tex (Dagger (Particle x)) = "p^{" ++ x ++ "}"
-  tex (Hole x) = "h_{" ++ x ++ "}"
-  tex (Dagger (Hole x)) = "h^{" ++ x ++ "}"
-  tex (FockOne) = "1"
-  tex (FockZero) = "0"
-  tex (Scaled s p) = (tex s) ++ "*" ++ (tex p)
-  tex (Oprod o p) = (tex o) ++ (tex p)
-  tex (Osum o p) = (tex o) ++ " + " ++ (tex p)
+  tex (Op       x           ) = "c_{" ++ x ++ "}"
+  tex (Dagger   (Op x)      ) = "c^{" ++ x ++ "}"
+  tex (Particle x           ) = "p_{" ++ x ++ "}"
+  tex (Dagger   (Particle x)) = "p^{" ++ x ++ "}"
+  tex (Hole     x           ) = "h_{" ++ x ++ "}"
+  tex (Dagger   (Hole x)    ) = "h^{" ++ x ++ "}"
+  tex FockOne = "1"
+  tex FockZero = "0"
+  tex (Scaled s p           ) = (tex s) ++ "*" ++ (tex p)
+  tex (Oprod  o p           ) = (tex o) ++ (tex p)
+  tex (Osum   o p           ) = (tex o) ++ " + " ++ (tex p)
 
 instance Tex Scalar where
-  tex (Delta x y) = "\\delta_{" ++ x ++ "," ++ y ++ "}"
-  tex (Val x) = "(" ++ (show x) ++ ")"
-  tex (Conj x) = "(\\bar{" ++ (show x) ++ "})"
-  tex (ScalarOne) = "1"
-  tex (ScalarZero) = "0"
-  tex (Sprod s t) = "(" ++ (tex s) ++ " * " ++ (tex t) ++ ")"
-  tex (Ssum s t) = "(" ++ (tex s) ++ " + " ++ (tex t) ++ ")"
-  tex (Tensor s a i) = s ++ "^{" ++ a ++ "}_{"++ i ++ "}"
+  tex (Delta x y   ) = "\\delta_{" ++ x ++ "," ++ y ++ "}"
+  tex (Val  x      ) = "(" ++ (show x) ++ ")"
+  tex (Conj x      ) = "(\\bar{" ++ (show x) ++ "})"
+  tex ScalarOne = "1"
+  tex ScalarZero = "0"
+  tex (Sprod s t   ) = "(" ++ (tex s) ++ " * " ++ (tex t) ++ ")"
+  tex (Ssum  s t   ) = "(" ++ (tex s) ++ " + " ++ (tex t) ++ ")"
+  tex (Tensor s a i) = s ++ "^{" ++ a ++ "}_{" ++ i ++ "}"
+  tex ParticleSign   = "ParticleSign"
 
 contract :: Operator -> Operator -> Scalar
 
-contract (Dagger (Hole x)) (Hole y) = (Delta x y)
-contract (Hole x) (Hole y) = ScalarZero
+contract (Dagger   (Hole x)) (Hole     y           ) = Delta x y
+contract (Hole     x       ) (Hole     y           ) = ScalarZero
 
-contract (Particle x) (Dagger (Particle y)) = (Delta x y)
-contract (Particle x) (Particle y) = ScalarZero
+contract (Particle x       ) (Dagger   (Particle y)) = Delta x y
+contract (Particle x       ) (Particle y           ) = ScalarZero
 
-contract (Particle x) (Hole y) = ScalarZero
-contract (Dagger (Hole x)) (Particle y) = ScalarZero
-contract (Hole x) (Dagger (Particle y)) = ScalarZero
+contract (Particle x       ) (Hole     y           ) = ScalarZero
+contract (Dagger   (Hole x)) (Particle y           ) = ScalarZero
+contract (Hole     x       ) (Dagger   (Particle y)) = ScalarZero
 
-contract (Oprod p q) (Oprod r s) =
-   foldl Ssum ScalarZero [Sprod (contract p q) (contract r s),
-                          Sprod (contract p r) (contract q s),
-                          Sprod (contract p s) (contract q r)]
-contract (o) (Oprod p s) = (Ssum (contract o p) (contract o s))
-contract (Scaled ScalarZero o) (p) = ScalarZero
-contract (Scaled s o) (p) = (Sprod s (contract o p))
-contract o p = contract p o
+contract (Oprod p q        ) (Oprod r s            ) = foldl
+  Ssum
+  ScalarZero
+  [ Sprod (contract p q) (contract r s)
+  , Sprod (contract p r) (contract q s)
+  , Sprod (contract p s) (contract q r)
+  ]
+contract o (Oprod p s) = Ssum (contract o p) (contract o s)
+contract (Scaled ScalarZero o) p           = ScalarZero
+contract (Scaled s          o) p           = Sprod s (contract o p)
+contract o                     p           = contract p o
--- test/QuantumFieldSpec.hs
+++ test/QuantumFieldSpec.hs
@@ -1,16 +1,18 @@
 import Test.Hspec
 import QuantumField
+import Prelude hiding ((*), (+))
 
+o idx = (Op idx)
 p idx = (Particle idx)
 p' idx = (dag (Particle idx))
 h idx = (Hole idx)
 h' idx = (dag (Hole idx))
-tai a i = Scaled (Tensor "t" a i) (p' a) # (h i)
-tabij a b i j = Scaled (Tensor "t" (a++b) (i++j)) (p' a) # (p' b) # (h j) # (h i)
-fij i j = Scaled (Tensor "f" i j) (h' j) # (h i)
-fab a b = Scaled (Tensor "f" a b) (p' a) # (p b)
-tauia i a = (h' i) # (p a)
-tauai a i = (p' a) # (h i)
+tai a i = Scaled (Tensor "t" a i) (p' a) * (h i)
+tabij a b i j = Scaled (Tensor "t" (a++b) (i++j)) (p' a) * (p' b) * (h j) * (h i)
+fij i j = Scaled (Tensor "f" i j) (h' j) * (h i)
+fab a b = Scaled (Tensor "f" a b) (p' a) * (p b)
+tauia i a = (h' i) * (p a)
+tauai a i = (p' a) * (h i)
 
 main :: IO ()
 main = hspec $ do
@@ -27,17 +29,35 @@ main = hspec $ do
       l = h "l"
       m = h "m"
       n = h "n"
-      a_i = (dag a) # i
-      i_a = (dag i) # a
-      tek = Scaled (Tensor "t" "b" "k") (dag b) # k
+      a_i = (dag a) * i
+      i_a = (dag i) * a
+      tek = Scaled (Tensor "t" "b" "k") (dag b) * k
 
   describe "TeX output" $ do
     it "Correctly outputs all particles and holes" $ do
+      (tex $ o "a") `shouldBe` "c_{a}"
+      (tex . dag $ o "a") `shouldBe` "c^{a}"
       (tex $ a) `shouldBe` "p_{a}"
       (tex $ i) `shouldBe` "h_{i}"
       (tex $ dag i) `shouldBe` "h^{i}"
-      (tex $ ((^+) a) # i) `shouldBe` "p^{a}h_{i}"
+      (tex $ ((^+) a) * i) `shouldBe` "p^{a}h_{i}"
       (tex $ tai "e" "k") `shouldBe` "t^{e}_{k}*p^{e}h_{k}"
+
+  describe "Build sums and products" $ do
+    it "General" $ do
+      (a + b)*c `shouldBe` Oprod (Osum a b) c
+
+  describe "Expanding operators" $ do
+    it "Expands sums" $ do
+      (expand a) `shouldBe` a
+      (expand $ a + b) `shouldBe` a + b
+      (expand $ (a + b)*c) `shouldBe` (a*c)+(b*c)
+      (expand $ c*(a + b)) `shouldBe` (c*a)+(c*b)
+      (expand $ (a + b)*(c + d)) `shouldBe` ((a*c)+(a*d))+((b*c)+(b*d))
+      (expand $ (a + b)*(c + d)*(i + j))
+        `shouldBe`
+         (a*(c+d)*(i+j))
+
 
   describe "Contraction" $ do
     it "Correctly contracts holes and particles" $ do
@@ -48,8 +68,9 @@ main = hspec $ do
       (tex $ contract i (dag a)) `shouldBe` "0"
     it ("Correctly contracts " ++ (tex a_i) ++ " and " ++ (tex i) ++ " particles") $ do
       (tex $ contract (a_i) i) `shouldBe` "(0 + 0)"
-    it ("Correctly contracts " ++ (tex a_i) ++ " and " ++ (tex i_a)) $ do
-      (tex $ contract a_i i_a) `shouldBe` "(((0 + (0 * 0)) + (0 * 0)) + (0 * 0))"
-    it ("Correctly contracts " ++ (tex i_a) ++ " and " ++ (tex a_i)) $ do
-      (tex $ contract i_a a_i) `shouldBe` "(((0 + (0 * 0)) + (0 * 0)) + (\\delta_{i,i} * \\delta_{a,a}))"
-      (tex $ contract (tauia "i" "a") ((fij "k" "l") # (tai "a" "m"))) `shouldBe` "asdf"
+    --it ("Correctly contracts " ++ (tex a_i) ++ " and " ++ (tex i_a)) $ do
+      --(tex $ contract a_i i_a) `shouldBe` "(((0 + (0 * 0)) + (0 * 0)) + (0 * 0))"
+    --it ("Correctly contracts " ++ (tex i_a) ++ " and " ++ (tex a_i)) $ do
+      --(tex $ contract i_a a_i) `shouldBe` "(((0 + (0 * 0)) + (0 * 0)) + (\\delta_{i,i} * \\delta_{a,a}))"
+      --(tex $ contract (tauia "i" "a") ((fij "k" "l") * (tai "a" "m"))) `shouldBe` "asdf"
+
