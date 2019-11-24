17e04506cf00de68b484d3e9d0c2ab44a7db6b14
Alejandro Gallo <aamsgallo@gmail.com>
2019-11-24 19:31:37 +0100

    Expand working better

--- src/QuantumField.hs
+++ src/QuantumField.hs
@@ -1,6 +1,12 @@
 module QuantumField where
 
-import Prelude hiding ((*), (+))
+import           Prelude                 hiding ( (*)
+                                                , (+)
+                                                , (**)
+                                                )
+import           Data.List
+import           Data.Monoid
+
 
 type Index = String
 type Field = Double
@@ -27,34 +33,52 @@ data Operator
   | Particle Index
   | Hole Index
   | Dagger Operator
-  | Oprod Operator Operator
-  | Osum Operator Operator
+  | Oprod [Operator]
+  | Osum [Operator]
   | Scaled Scalar Operator
   deriving(Show, Eq)
 
+
+
 class Ring a where
   (*) :: a -> a -> a
+  (**) :: a -> Int -> a
   (+) :: a -> a -> a
 
 instance Ring Operator where
-  (*) = Oprod
-  (+) = Osum
+  FockOne   * a         = a
+  a         * FockOne   = a
+  (Oprod a) * (Oprod b) = Oprod $ a ++ b
+  (Oprod a) * b         = Oprod $ a ++ [b]
+  b         * (Oprod a) = Oprod $ b : a
+  a         * b         = Oprod [a, b]
+
+  a ** n | n <= 0    = Oprod []
+         | otherwise = a * (a ** (n - 1))
+
+
+  b        + FockZero = b
+  FockZero + b        = b
+  (Osum a) + (Osum b) = Osum $ a ++ b
+  (Osum a) + b        = Osum $ a ++ [b]
+  b        + (Osum a) = Osum $ b : a
+  a        + b        = Osum [a, b]
 
 conj :: Scalar -> Scalar
-conj (Delta s t)    = Delta s t
-conj (Val  s   )    = Conj s
-conj (Conj s   )    = Val s
-conj ScalarZero     = ScalarZero
-conj ScalarOne      = ScalarOne
+conj (Delta s t   ) = Delta s t
+conj (Val  s      ) = Conj s
+conj (Conj s      ) = Val s
 conj (Sprod s t   ) = Sprod (conj s) (conj t)
 conj (Ssum  s t   ) = Ssum (conj s) (conj t)
 conj (Tensor s a i) = Tensor s a i
+conj ScalarZero     = ScalarZero
+conj ScalarOne      = ScalarOne
 
 dag :: Operator -> Operator
 dag (Dagger (Particle i)) = Particle i
 dag (Dagger (Hole     a)) = Hole a
-dag (Oprod  a b         ) = Oprod (dag b) (dag a)
-dag (Scaled s o         ) = Scaled s (dag o)
+dag (Oprod  a           ) = Oprod (map dag $ reverse a)
+dag (Scaled s o         ) = Scaled s $ dag o
 dag FockOne               = FockOne
 dag FockZero              = FockZero
 dag o                     = Dagger o
@@ -66,33 +90,37 @@ isOneBody (Particle x           ) = True
 isOneBody (Dagger   (Particle x)) = True
 isOneBody (Hole     x           ) = True
 isOneBody (Dagger   (Hole x)    ) = True
-isOneBody (Scaled   s p    ) = isOneBody p
-isOneBody (Osum p q) = isOneBody p && isOneBody q
-isOneBody  _ = False
+isOneBody (Scaled s p           ) = isOneBody p
+isOneBody (Osum q               ) = all isOneBody q
+isOneBody _                       = False
 
 normalize :: Operator -> Operator
-normalize (Oprod (Op b) (Dagger (Op a))) = Scaled ParticleSign o
-  where o = Oprod (Dagger (Op a)) (Op b)
-normalize (Oprod b (Dagger (Op a))) = Scaled ParticleSign o
-  where o = Oprod (Dagger (Op a)) (normalize b)
-normalize (Oprod b (Op a)) = Oprod (normalize b) (Op a)
-normalize (Oprod (Particle b) (Dagger (Particle a))) = Scaled ParticleSign o
-  where o = Oprod (Dagger (Particle a)) (Particle b)
-normalize (Oprod (Dagger (Hole a)) (Hole b)) = Scaled ParticleSign o
-  where o = Oprod (Hole b) (Dagger (Hole a))
-normalize (Osum   a b) = Osum (normalize a) (normalize b)
+--normalize (Oprod (Op b) (Dagger (Op a))) = Scaled ParticleSign o
+  --where o = Oprod (Dagger (Op a)) (Op b)
+--normalize (Oprod b (Dagger (Op a))) = Scaled ParticleSign o
+  --where o = Oprod (Dagger (Op a)) (normalize b)
+--normalize (Oprod b (Op a)) = Oprod (normalize b) (Op a)
+--normalize (Oprod (Particle b) (Dagger (Particle a))) = Scaled ParticleSign o
+  --where o = Oprod (Dagger (Particle a)) (Particle b)
+--normalize (Oprod (Dagger (Hole a)) (Hole b)) = Scaled ParticleSign o
+  --where o = Oprod (Hole b) (Dagger (Hole a))
+--normalize (Osum   a b) = Osum (normalize a) (normalize b)
 normalize (Scaled a b) = Scaled a (normalize b)
 normalize a            = a
 
 expand :: Operator -> Operator
-expand (Oprod (Osum a b) c) = Osum (expand (Oprod a c)) (expand (Oprod b c))
-expand (Oprod c (Osum a b)) = Osum (expand (Oprod c a)) (expand (Oprod c b))
---expand (Oprod a b) = Qprod (expand a) (expand b)
-expand (Osum a b) = Osum (expand a) (expand b)
-expand c = c
+expand (Oprod c) = Osum $ map Oprod (expandList c)
+expand (Osum a) = foldl (+) FockZero (map (expand . Oprod . (:[])) a)
+--expand (Osum a) = Osum $ map (expandList . (:[])) a
+expand (Scaled s a) = Osum [Scaled s (expand a)]
+expand c        = Osum [c]
 
-(^+) :: Operator -> Operator
-(^+) = dag
+expandList :: [Operator] -> [[Operator]]
+expandList l@(x : xs) = case x of
+  (Oprod a) -> expandList $ a++xs
+  (Osum a) -> mconcat $ map expandList $ [(:)] <*> a <*> [xs]
+  _ -> map (x :) (expandList xs)
+expandList [] = [[]]
 
 instance Tex Operator where
   tex (Op       x           ) = "c_{" ++ x ++ "}"
@@ -101,43 +129,43 @@ instance Tex Operator where
   tex (Dagger   (Particle x)) = "p^{" ++ x ++ "}"
   tex (Hole     x           ) = "h_{" ++ x ++ "}"
   tex (Dagger   (Hole x)    ) = "h^{" ++ x ++ "}"
-  tex FockOne = "1"
-  tex FockZero = "0"
-  tex (Scaled s p           ) = (tex s) ++ "*" ++ (tex p)
-  tex (Oprod  o p           ) = (tex o) ++ (tex p)
-  tex (Osum   o p           ) = (tex o) ++ " + " ++ (tex p)
+  tex FockOne                 = "1"
+  tex FockZero                = "0"
+  tex (Scaled s p)            = tex s ++ "*" ++ tex p
+  tex (Oprod o   )            = mconcat $ map tex o
+  tex (Osum  o   )            = intercalate " + " $ map tex o
 
 instance Tex Scalar where
-  tex (Delta x y   ) = "\\delta_{" ++ x ++ "," ++ y ++ "}"
-  tex (Val  x      ) = "(" ++ (show x) ++ ")"
-  tex (Conj x      ) = "(\\bar{" ++ (show x) ++ "})"
-  tex ScalarOne = "1"
-  tex ScalarZero = "0"
-  tex (Sprod s t   ) = "(" ++ (tex s) ++ " * " ++ (tex t) ++ ")"
-  tex (Ssum  s t   ) = "(" ++ (tex s) ++ " + " ++ (tex t) ++ ")"
+  tex (Delta x y)    = "\\delta_{" ++ x ++ "," ++ y ++ "}"
+  tex (Val  x   )    = "(" ++ show x ++ ")"
+  tex (Conj x   )    = "(\\bar{" ++ show x ++ "})"
+  tex ScalarOne      = "1"
+  tex ScalarZero     = "0"
+  tex (Sprod s t   ) = "(" ++ tex s ++ " * " ++ tex t ++ ")"
+  tex (Ssum  s t   ) = "(" ++ tex s ++ " + " ++ tex t ++ ")"
   tex (Tensor s a i) = s ++ "^{" ++ a ++ "}_{" ++ i ++ "}"
   tex ParticleSign   = "ParticleSign"
 
 contract :: Operator -> Operator -> Scalar
 
-contract (Dagger   (Hole x)) (Hole     y           ) = Delta x y
-contract (Hole     x       ) (Hole     y           ) = ScalarZero
+contract (Dagger   (Hole x)  ) (Hole     y           ) = Delta x y
+contract (Hole     x         ) (Hole     y           ) = ScalarZero
 
-contract (Particle x       ) (Dagger   (Particle y)) = Delta x y
-contract (Particle x       ) (Particle y           ) = ScalarZero
+contract (Particle x         ) (Dagger   (Particle y)) = Delta x y
+contract (Particle x         ) (Particle y           ) = ScalarZero
 
-contract (Particle x       ) (Hole     y           ) = ScalarZero
-contract (Dagger   (Hole x)) (Particle y           ) = ScalarZero
-contract (Hole     x       ) (Dagger   (Particle y)) = ScalarZero
+contract (Particle x         ) (Hole     y           ) = ScalarZero
+contract (Dagger   (Hole x)  ) (Particle y           ) = ScalarZero
+contract (Hole     x         ) (Dagger   (Particle y)) = ScalarZero
 
-contract (Oprod p q        ) (Oprod r s            ) = foldl
-  Ssum
-  ScalarZero
-  [ Sprod (contract p q) (contract r s)
-  , Sprod (contract p r) (contract q s)
-  , Sprod (contract p s) (contract q r)
-  ]
-contract o (Oprod p s) = Ssum (contract o p) (contract o s)
-contract (Scaled ScalarZero o) p           = ScalarZero
-contract (Scaled s          o) p           = Sprod s (contract o p)
-contract o                     p           = contract p o
+--contract (Oprod p q        ) (Oprod r s            ) = foldl
+  --Ssum
+  --ScalarZero
+  --[ Sprod (contract p q) (contract r s)
+  --, Sprod (contract p r) (contract q s)
+  --, Sprod (contract p s) (contract q r)
+  --]
+--contract o (Oprod p s) = Ssum (contract o p) (contract o s)
+contract (Scaled ScalarZero o) p                       = ScalarZero
+contract (Scaled s          o) p                       = Sprod s (contract o p)
+contract o                     p                       = contract p o
--- test/QuantumFieldSpec.hs
+++ test/QuantumFieldSpec.hs
@@ -1,18 +1,18 @@
 import Test.Hspec
 import QuantumField
-import Prelude hiding ((*), (+))
+import Prelude hiding ((*), (**), (+))
 
-o idx = (Op idx)
-p idx = (Particle idx)
-p' idx = (dag (Particle idx))
-h idx = (Hole idx)
-h' idx = (dag (Hole idx))
-tai a i = Scaled (Tensor "t" a i) (p' a) * (h i)
-tabij a b i j = Scaled (Tensor "t" (a++b) (i++j)) (p' a) * (p' b) * (h j) * (h i)
-fij i j = Scaled (Tensor "f" i j) (h' j) * (h i)
-fab a b = Scaled (Tensor "f" a b) (p' a) * (p b)
-tauia i a = (h' i) * (p a)
-tauai a i = (p' a) * (h i)
+o = Op
+p = Particle
+p' idx = dag (Particle idx)
+h = Hole
+h' idx = dag (Hole idx)
+tai a i = Scaled (Tensor "t" a i) $ p' a * h i
+tabij a b i j = Scaled (Tensor "t" (a++b) (i++j)) $ p' a * p' b * h j * h i
+fij i j = Scaled (Tensor "f" i j) $ h' j * h i
+fab a b = Scaled (Tensor "f" a b) $ p' a * p b
+tauia i a = h' i * p a
+tauai a i = p' a * h i
 
 main :: IO ()
 main = hspec $ do
@@ -29,35 +29,53 @@ main = hspec $ do
       l = h "l"
       m = h "m"
       n = h "n"
-      a_i = (dag a) * i
-      i_a = (dag i) * a
-      tek = Scaled (Tensor "t" "b" "k") (dag b) * k
+      a_i = dag a * i
+      i_a = dag i * a
+      tek = Scaled (Tensor "t" "b" "k") $ dag b * k
 
   describe "TeX output" $ do
     it "Correctly outputs all particles and holes" $ do
-      (tex $ o "a") `shouldBe` "c_{a}"
+      tex (o "a") `shouldBe` "c_{a}"
       (tex . dag $ o "a") `shouldBe` "c^{a}"
-      (tex $ a) `shouldBe` "p_{a}"
-      (tex $ i) `shouldBe` "h_{i}"
-      (tex $ dag i) `shouldBe` "h^{i}"
-      (tex $ ((^+) a) * i) `shouldBe` "p^{a}h_{i}"
-      (tex $ tai "e" "k") `shouldBe` "t^{e}_{k}*p^{e}h_{k}"
+      tex a `shouldBe` "p_{a}"
+      tex i `shouldBe` "h_{i}"
+      tex (dag i) `shouldBe` "h^{i}"
+      tex (dag a * i) `shouldBe` "p^{a}h_{i}"
+      tex (a * b * c) `shouldBe` "p_{a}p_{b}p_{c}"
+      tex (a*i + b + c) `shouldBe` "p_{a}h_{i} + p_{b} + p_{c}"
+      tex (a*dag i + b + c) `shouldBe` "p_{a}h^{i} + p_{b} + p_{c}"
+      tex (tai "e" "k") `shouldBe` "t^{e}_{k}*p^{e}h_{k}"
 
   describe "Build sums and products" $ do
     it "General" $ do
-      (a + b)*c `shouldBe` Oprod (Osum a b) c
+      a*c `shouldBe` Oprod [a, c]
+      (a + b)*c `shouldBe` Oprod [a+b, c]
+      c*(a + b) `shouldBe` Oprod [c, a+b]
+      (a + b + c + d) `shouldBe` Osum [a,b,c,d]
+      (a ** 3) `shouldBe` Oprod [a,a,a]
 
   describe "Expanding operators" $ do
-    it "Expands sums" $ do
-      (expand a) `shouldBe` a
-      (expand $ a + b) `shouldBe` a + b
-      (expand $ (a + b)*c) `shouldBe` (a*c)+(b*c)
-      (expand $ c*(a + b)) `shouldBe` (c*a)+(c*b)
-      (expand $ (a + b)*(c + d)) `shouldBe` ((a*c)+(a*d))+((b*c)+(b*d))
-      (expand $ (a + b)*(c + d)*(i + j))
-        `shouldBe`
-         (a*(c+d)*(i+j))
-
+    it "trivial expand" $ do
+      expand a `shouldBe` Osum [a]
+      expand (a + b) `shouldBe` (Oprod [a]) + (Oprod [b])
+      expand (a + b + c) `shouldBe` (Oprod [a]) + (Oprod [b]) + (Oprod [c])
+      expand (a*b) `shouldBe` Osum [a*b]
+      expand ((a*b) + (b*i) + c) `shouldBe` Osum [a*b, b*i, Oprod [c]]
+    it "Expands sums (a+b)**2" $ do
+      expand ((a + b)**2) `shouldBe` Osum [a**2, a*b, b*a, b**2]
+    it "Expands sums (a+b)*c" $ do
+      expand ((a + b)*c) `shouldBe` Osum [a*c, b*c]
+    it "Expands sums c*(a+b)" $ do
+      expand (c*(a + b)) `shouldBe` (c*a)+(c*b)
+    it "Expands sums (a+b)*(c+d)" $ do
+      expand ((a + b)*(c + d)) `shouldBe` ((a*c)+(a*d))+((b*c)+(b*d))
+    it "Expands sums (a+b)*(c+d)*i" $ do
+      expand ((a + b)*(c + d)*i) `shouldBe` Osum [a*c*i,a*d*i,b*c*i,b*d*i]
+    it "Expands sums (a+b)*i*(c+d)" $ do
+      expand ((a + b)*i*(c + d)) `shouldBe` Osum [a*i*c,a*i*d,b*i*c,b*i*d]
+    it "Expands sums i*(a+b)*(c+d)" $ do
+      expand (i*(a + b)*(c + d)) `shouldBe` Osum [i*a*c,i*a*d,i*b*c,i*b*d]
+{-
 
   describe "Contraction" $ do
     it "Correctly contracts holes and particles" $ do
@@ -74,3 +92,4 @@ main = hspec $ do
       --(tex $ contract i_a a_i) `shouldBe` "(((0 + (0 * 0)) + (0 * 0)) + (\\delta_{i,i} * \\delta_{a,a}))"
       --(tex $ contract (tauia "i" "a") ((fij "k" "l") * (tai "a" "m"))) `shouldBe` "asdf"
 
+-}
